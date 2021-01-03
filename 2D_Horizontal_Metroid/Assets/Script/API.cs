c8e14f2771315675ce8fef5ae2a33ab723309cbd
jerry7700 <75553251+jerry7700@users.noreply.github.com>
2021-01-03 13:02:10 +0800

    非靜態 切換場景

---
+++ 2D_Horizontal_Metroid/Assets/Script/API.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class API : MonoBehaviour
+{
+    //修飾詞 類別 欄位
+    public Transform tarA;
+    public Transform Tester;
+    public Camera cam;
+    public SpriteRenderer sp;
+    public GameObject lr;
+
+    private void Start()
+    {
+        #region 非靜態
+        //非靜態
+        //取得:
+        //欄位.屬性
+        print("座標" + tarA.position);
+
+        //設定:
+        //欄位.屬性 指定 值
+        Tester.position = new Vector3(2, 0, 0);
+
+        //靜態屬性
+        print("攝影機數量:" + Camera.allCameras);
+
+        cam.backgroundColor = new Color(0.5f, 0.2f, 0.3f);
+        #endregion
+        #region 練習
+        //取得
+        print("顏色:" + sp.color);
+        print("圖層:" + lr.layer);
+        //設定
+        sp.flipX = false;
+        lr.layer = 5;
+        #endregion
+    }
+
+    private void Update()
+    {
+        //非靜態
+        //取得:
+        //欄位.方法(對應的引數)
+        Tester.Rotate(0, 0, 30);
+        //位移(X,Y,Z,空間)
+        //world 世界
+        //self 區域
+        Tester.Translate(0.5f, 0f, 0f, Space.World);
+    }
+}
---
+++ 2D_Horizontal_Metroid/Assets/Script/SceneControl.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement; //引用 場景管理 API
+
+public class SceneControl : MonoBehaviour
+{
+    //1.方法要讓按鈕呼叫必須設為公開 public
+    /// <summary>
+    /// 開始遊戲
+    /// </summary>
+   public void StartGame()
+    {
+        //2.必須將場景放在 file > Build Settings...
+        //場景管理.載入場景("場景名稱")
+        SceneManager.LoadScene("遊戲場景");
+    }
+
+    /// <summary>
+    /// 回選單
+    /// </summary>
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("選單");
+    }
+
+    /// <summary>
+    /// 離開遊戲
+    /// </summary>
+    public void QuitGame()
+    {
+        //應用程式.離開遊戲()
+        Application.Quit();
+    }
+}
