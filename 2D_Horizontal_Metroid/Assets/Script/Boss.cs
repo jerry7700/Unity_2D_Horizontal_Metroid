b1cd6a67aba4888b9410b82cd080266c981c8d9a
jerry7700 <75553251+jerry7700@users.noreply.github.com>
2021-01-17 17:02:44 +0800

    Boss受傷

---
+++ 2D_Horizontal_Metroid/Assets/Script/Boss.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+
+[RequireComponent(typeof(AudioSource), typeof(Rigidbody2D), typeof(CapsuleCollider2D))]
+public class Boss : MonoBehaviour
+{
+    [Header("移動速度")]
+    [Range(0, 1000)]
+    public float Speed = 10.5f;
+    [Header("攻擊範圍")]
+    [Range(0, 100)]
+    public float rangestr = 100;
+    [Header("攻擊力")]
+    [Range(0, 1000)]
+    public float str = 100;
+    [Header("血量")]
+    [Range(0, 5000)]
+    public float HP = 100;
+    [Header("音效來源")]
+    private AudioSource aud;
+    [Header("2D 剛體")]
+    private Rigidbody2D rb;
+    [Header("動畫控制")]
+    private Animator anim;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        aud = GetComponent<AudioSource>();
+    }
+
+    public void Damage(float damage)
+    {
+        HP -= damage;
+        anim.SetTrigger("受傷觸發");
+    }
+}
---
+++ 2D_Horizontal_Metroid/Assets/Script/Bullet.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public int attack;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.GetComponent<Boss>())
+        {
+            collision.gameObject.GetComponent<Boss>().Damage(attack);
+        }
+    }
+}
